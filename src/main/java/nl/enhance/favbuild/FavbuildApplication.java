acd9dfa4d4894c0e54b099380efb35135805db49
Hans Westerbeek <hans@enhance-ict.com>
2017-11-06 20:49:44 +0100

    Initial

---
+++ src/main/java/nl/enhance/favbuild/FavbuildApplication.java
@@ -0,0 +1,12 @@
+package nl.enhance.favbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavbuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavbuildApplication.class, args);
+	}
+}
