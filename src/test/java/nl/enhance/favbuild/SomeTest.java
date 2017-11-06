876d80f7fe736dcf6e248d7ebdc52820c8d77a35
Hans Westerbeek <hans@enhance-ict.com>
2017-11-06 21:19:43 +0100

    meer

---
+++ src/test/java/nl/enhance/favbuild/SomeTest.java
@@ -0,0 +1,14 @@
+package nl.enhance.favbuild;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SomeTest {
+
+    @Test
+    public void foo() {
+        assertTrue(true);
+    }
+
+}
