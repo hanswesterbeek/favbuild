6b0d0fa1fdfa6847f85973ced31ae2311e2c6c93
Hans Westerbeek <hans@enhance-ict.com>
2017-11-06 21:10:08 +0100

    Add iT and fT

---
+++ src/test/java/nl/enhance/favbuild/DemoFunctionalTest.java
@@ -0,0 +1,14 @@
+package nl.enhance.favbuild;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DemoFunctionalTest {
+
+    @Test
+    public void foo() {
+        assertTrue(true);
+    }
+
+}
--- src/test/java/nl/enhance/favbuild/FavbuildApplicationIntegrationTest.java
+++ src/test/java/nl/enhance/favbuild/FavbuildApplicationIntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FavbuildApplicationTests {
+public class FavbuildApplicationIntegrationTest {
 
 	@Test
 	public void contextLoads() {
