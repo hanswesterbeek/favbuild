8ef1e09e17b04bf7619572c0e2c59ac7ff5bee68
Hans Westerbeek <hans@enhance-ict.com>
2017-11-06 21:15:57 +0100

    Provide db config during dev.

--- src/test/java/nl/enhance/favbuild/FavbuildIntegrationTest.java
+++ src/test/java/nl/enhance/favbuild/FavbuildIntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FavbuildApplicationIntegrationTest {
+public class FavbuildIntegrationTest {
 
 	@Test
 	public void contextLoads() {
