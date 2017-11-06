294929cfdb007cee554633e5d9432d57b96528a9
Hans Westerbeek <hans@enhance-ict.com>
2017-11-06 22:46:33 +0100

    Add fT that passes.

---
+++ src/main/java/nl/enhance/favbuild/FooController.java
@@ -0,0 +1,17 @@
+package nl.enhance.favbuild;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FooController {
+
+    @RequestMapping(value = "/foo")
+    public Map<String, String> bar() {
+
+        return Collections.singletonMap("foo", "bar");
+    }
+}
--- src/test/java/nl/enhance/favbuild/DemoFunctionalTest.java
+++ src/test/java/nl/enhance/favbuild/DemoFunctionalTest.java
@@ -1,14 +1,32 @@
 package nl.enhance.favbuild;
 
+import static io.restassured.RestAssured.when;
 import static org.junit.Assert.assertTrue;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 
 public class DemoFunctionalTest {
 
     @Test
-    public void foo() {
-        assertTrue(true);
+    public void foo() throws MalformedURLException {
+
+        String baseUrl = System.getProperty("app.url");
+        final String url = baseUrl + "/foo";
+        final Response response =
+                when().get(new URL(url)).then()
+                        .assertThat().statusCode(200)
+                        .assertThat().contentType(ContentType.JSON).extract()
+                .response();
+        JsonPath jsonPath = new JsonPath(response.asInputStream());
+        final String result = jsonPath.getString("foo");
+        assertTrue(("bar".equals(result)));
     }
 
 }
