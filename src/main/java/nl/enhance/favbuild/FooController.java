1457a2e3179173b7c4c2fc40878a14da6bb0ee48
Hans Westerbeek <hans@enhance-ict.com>
2017-11-28 16:47:13 +0100

    add some faults

--- src/main/java/nl/enhance/favbuild/FooController.java
+++ src/main/java/nl/enhance/favbuild/FooController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FooController {
