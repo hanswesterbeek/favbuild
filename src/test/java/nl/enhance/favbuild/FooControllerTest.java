5f212e79de4a80dd9a4666bfe8c8b05c72f49b67
Hans Westerbeek <hans@enhance-ict.com>
2017-11-28 16:43:39 +0100

    add some faults

---
+++ src/test/java/nl/enhance/favbuild/FooControllerTest.java
@@ -0,0 +1,18 @@
+package nl.enhance.favbuild;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FooControllerTest {
+
+  @Before
+  public void setup() throws Exception {
+  }
+
+  @Test
+  public void foo() {
+
+  }
+}
